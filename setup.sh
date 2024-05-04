3cacd4ef599fc3a7ef5d229649162a1f087a6bb1
Mike Di Domenico <3809319+didomenicom@users.noreply.github.com>
2024-05-04 17:14:01 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -143,6 +143,8 @@ defaults write com.apple.dock wvous-tl-corner -int 4
 defaults write com.apple.dock wvous-tl-modifier -int 0
 defaults write com.apple.dock wvous-tr-corner -int 2
 defaults write com.apple.dock wvous-tr-modifier -int 0
+defaults write com.apple.dock wvous-br-corner -int 0
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 killall Dock
 
